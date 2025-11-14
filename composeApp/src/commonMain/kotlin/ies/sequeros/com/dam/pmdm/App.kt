89f1215244d0952a77c3497db5c52acf9adf1b9c
Javier <javpomgom@alu.edu.gva.es>
2025-11-14 19:21:39 +0100

    Primer commit.

---
+++ composeApp/src/commonMain/kotlin/ies/sequeros/com/dam/pmdm/App.kt
@@ -0,0 +1,54 @@
+package ies.sequeros.com.dam.pmdm
+
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ies.sequeros.com.dam.pmdm.administrador.AdministradorViewModel
+import ies.sequeros.com.dam.pmdm.commons.infraestructura.AlmacenDatos
+import ies.sequeros.com.dam.pmdm.administrador.modelo.IDependienteRepositorio
+
+import ies.sequeros.com.dam.pmdm.administrador.ui.MainAdministrador
+import ies.sequeros.com.dam.pmdm.administrador.ui.MainAdministradorViewModel
+import ies.sequeros.com.dam.pmdm.administrador.ui.dependientes.DependientesViewModel
+
+@Suppress("ViewModelConstructorInComposable")
+@Composable
+
+fun App( dependienteRepositorio : IDependienteRepositorio,almacenImagenes:AlmacenDatos) {
+
+    //view model
+    val appViewModel= viewModel {  AppViewModel() }
+    val mainViewModel= remember { MainAdministradorViewModel() }
+    val administradorViewModel= viewModel { AdministradorViewModel() }
+    val dependientesViewModel = viewModel{ DependientesViewModel(
+        dependienteRepositorio, almacenImagenes
+    )}
+
+    appViewModel.setWindowsAdatativeInfo( currentWindowAdaptiveInfo())
+    val navController= rememberNavController()
+//para cambiar el modo
+    AppTheme(appViewModel.darkMode.collectAsState()) {
+
+        NavHost(
+            navController,
+            startDestination = AppRoutes.Main
+        ) {
+            composable(AppRoutes.Main) {
+                Principal({
+                    navController.navigate(AppRoutes.Administrador)
+                },{},{})
+            }
+            composable (AppRoutes.Administrador){
+                MainAdministrador(appViewModel,mainViewModel,administradorViewModel,
+                    dependientesViewModel,{
+                    navController.popBackStack()
+                })
+            }
+
+        }
+    }
+
+}
